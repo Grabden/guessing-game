42d667771f7139d68079eb382a6185a5e5b082b4
Dolgor <budatsyrenova.dolgor@mail.ru>
2023-02-27 21:10:34 +0800

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
     constructor() {}
-
+  
     setRange(min, max) {
-
+      this.average= Math.round((min+max)/2);
+      this.min=min;
+      this.max=max;
     }
-
+  
     guess() {
-
+      return this.average;
     }
-
+  
     lower() {
-
+      this.setRange(this.min,this.average);
+      //console.log('max',this.max);
     }
-
+  
     greater() {
-
+      this.setRange(this.average,this.max);
+      //console.log('min',this.min);
     }
-}
-
+  }
 module.exports = GuessingGame;
